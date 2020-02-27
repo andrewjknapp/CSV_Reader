7b0cf8071e234654169bc07452e21a9659636178
Armadillo <ajknapp2015@gmail.com>
2020-02-26 16:35:01 -0800

    Added main handlebars template

--- csvParse.js
+++ csvParse.js
@@ -1,11 +1,16 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 
-fs.createReadStream('PRECIP_HLY_sample_csv.csv')
+let dataPath = '/Accidental_Drug_Related_Deaths_2012-2018.csv';
+
+let result = [];
+
+fs.createReadStream(__dirname + '/data' + dataPath)
   .pipe(csv())
   .on('data', (row) => {
-    console.log(row);
+    result.push(row);
   })
   .on('end', () => {
+    console.log(result)
     console.log('CSV file successfully processed');
   });
--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const exphbs = require('expres-handlebars')
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+app.listen(PORT, function(err) {
+    if (err) throw err;
+    console.log("Server listening at http://localhost:8080");
+})
